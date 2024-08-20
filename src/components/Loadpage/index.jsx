275ba5b7016ec1a4a01a3276f3277895bb7f4e03
Milla22 <marti.ludmilla@orange.fr>
2024-08-20 10:24:19 +0200

    loadpage

---
+++ src/components/Loadpage/index.jsx
@@ -0,0 +1,12 @@
+import "./loadPage.css";
+
+function LoadPage() {
+    return (
+        <div className="container-loader">
+            <div className="ring">
+                <h3>Loading...</h3>
+            </div>
+        </div>
+    )
+}
+export default LoadPage;
